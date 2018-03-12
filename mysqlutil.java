727b01bb80d0008637429e2bc65b97e0ee2158bb
infcnwangjie <wangjie_rj@163.com>
2018-03-12 18:49:17 +0800

    mysql

---
+++ mysqlutil.java
@@ -0,0 +1,34 @@
+//关于端口 用户名密码在mysql中测试后填写
+public class GetConnection {
+    private Connection connection =null;
+    //连接emptyclassroom数据库
+    private String DatabaseName="emptyclassroom";
+
+    public static final String name = "com.mysql.jdbc.Driver";  
+    public static final String user = "数据库用户名";//默认root  
+    public static final String password = "数据库密码"; //默认root 
+    public Connection getCon(){
+        String url = "jdbc:mysql://localhost:3306/"+DatabaseName+"?useUnicode=true&characterEncoding=utf8&useSSL=true";
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection=DriverManager.getConnection(url,user,password);
+//          System.out.println(connection.getMetaData().getURL());
+
+            return connection;
+        }catch(Exception e){
+            System.out.println("创建连接失败");
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void closeCon(){
+            try { 
+                this.connection.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                System.out.println("关闭连接失败");
+                e.printStackTrace();
+            }
+    }
+}
